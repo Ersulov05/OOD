1e01a6c522386102e6ece4d0ad5a831a00da431d
Ersulov05 <ersulov05@mail.ru>
2025-10-06 11:29:44 +0300

    lw3-t1-coffee Увеличил ассортимент напитков

--- lw3/task1/lib/Beverages.h
+++ lw3/task1/lib/Beverages.h
@@ -39,38 +39,54 @@ public:
 class CCappuccino : public CCoffee
 {
 public:
-	CCappuccino()
-		: CCoffee("Cappuccino")
+	CCappuccino(bool isDouble = false)
+		: CCoffee(isDouble ? "Double Cappuccino" : "Cappuccino")
+		, m_isDouble(isDouble)
 	{
 	}
 
 	double GetCost() const override
 	{
-		return 80;
+		return m_isDouble ? 120 : 80;
 	}
+
+private:
+	bool m_isDouble;
 };
 
 // Латте
 class CLatte : public CCoffee
 {
 public:
-	CLatte()
-		: CCoffee("Latte")
+	CLatte(bool isDouble = false)
+		: CCoffee(isDouble ? "Double Latte" : "Latte")
+		, m_isDouble(isDouble)
 	{
 	}
 
 	double GetCost() const override
 	{
-		return 90;
+		return m_isDouble ? 130 : 90;
 	}
+
+private:
+	bool m_isDouble;
+};
+
+enum class TeaType
+{
+	Black,
+	Green,
+	Red,
+	White
 };
 
 // Чай
 class CTea : public CBeverage
 {
 public:
-	CTea()
-		: CBeverage("Tea")
+	CTea(TeaType teaType = TeaType::Black)
+		: CBeverage(GetTeaTypeName(teaType) + " tea")
 	{
 	}
 
@@ -78,19 +94,78 @@ public:
 	{
 		return 30;
 	}
+
+private:
+	static std::string GetTeaTypeName(TeaType teaType)
+	{
+		switch (teaType)
+		{
+		case TeaType::Black:
+			return "Black";
+		case TeaType::Green:
+			return "Green";
+		case TeaType::Red:
+			return "Red";
+		case TeaType::White:
+			return "White";
+		default:
+			throw std::runtime_error("Unknown TeaType");
+		}
+	}
+};
+
+enum class MilkshakeSize
+{
+	Small,
+	Medium,
+	Large
 };
 
 // Молочный коктейль
 class CMilkshake : public CBeverage
 {
 public:
-	CMilkshake()
-		: CBeverage("Milkshake")
+	CMilkshake(MilkshakeSize size = MilkshakeSize::Large)
+		: CBeverage(GetMilkshakeSizeName(size) + " milkshake")
+		, m_size(size)
 	{
 	}
 
 	double GetCost() const override
 	{
-		return 80;
+		return GetMilkshakeSizeCost(m_size);
+	}
+
+private:
+	MilkshakeSize m_size;
+
+	static std::string GetMilkshakeSizeName(MilkshakeSize size)
+	{
+		switch (size)
+		{
+		case MilkshakeSize::Small:
+			return "Small";
+		case MilkshakeSize::Medium:
+			return "Medium";
+		case MilkshakeSize::Large:
+			return "Large";
+		default:
+			throw std::runtime_error("Unknown Milkshake size");
+		}
+	}
+
+	static double GetMilkshakeSizeCost(MilkshakeSize size)
+	{
+		switch (size)
+		{
+		case MilkshakeSize::Small:
+			return 50;
+		case MilkshakeSize::Medium:
+			return 60;
+		case MilkshakeSize::Large:
+			return 80;
+		default:
+			throw std::runtime_error("Unknown Milkshake size");
+		}
 	}
 };
